4ebcc56d0e45938c482c34944159250a677f9428
PUF1 <goran@puf.hr>
2015-05-11 14:18:40 +0200

    sidebar, nav top bottom, I gump

---
+++ main.js
@@ -0,0 +1,7 @@
+   $("#sidebar-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        $("#sidebar-toggle").toggleClass("toggled");
+        
+        
+    });
